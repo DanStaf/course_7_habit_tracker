db7edb5cb2112bc6dc90f425da7c692e96687442
DanStaf <danstaf@mail.ru>
2024-12-05 14:07:12 +0300

    setup public habits (view and access)

--- spa/views.py
+++ spa/views.py
@@ -1,3 +1,4 @@
+import rest_framework.permissions
 from django.shortcuts import render
 from rest_framework import viewsets
 
@@ -7,10 +8,13 @@ from spa.serializers import HabitSerializer
 from users.permissions import IsOwnerClass
 
 
-# Create your views here.
-
-
 class HabitViewSet(viewsets.ModelViewSet):
+    """
+    owner can view their own list and retrieve
+    owner can update and delete
+    all can create
+    """
+
     serializer_class = HabitSerializer
     pagination_class = MyPagination
 
@@ -26,5 +30,27 @@ class HabitViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action in ['update', 'partial_update', 'destroy']:
             self.permission_classes = [IsOwnerClass]
+
+        return super().get_permissions()
+
+
+class PublicViewSet(viewsets.ModelViewSet):
+    """
+    all can view list and retrieve
+    owner can update and delete
+    no one can create
+    """
+    serializer_class = HabitSerializer
+    pagination_class = MyPagination
+
+    def get_queryset(self):
+        queryset = Habit.objects.filter(is_public=True)
+        return queryset
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsOwnerClass]
+        elif self.action in ['create']:
+            self.permission_classes = [~rest_framework.permissions.AllowAny]
 
         return super().get_permissions()
