c230ce8983cdab94ab8128983ce7819655aae506
DanStaf <danstaf@mail.ru>
2024-12-06 15:28:59 +0300

    validation added

--- spa/validators.py
+++ spa/validators.py
@@ -1,23 +1,5 @@
 from rest_framework import serializers
 
-"""
-Исключить одновременный выбор связанной привычки и указания вознаграждения.
-В модели не должно быть заполнено одновременно и поле вознаграждения,
-и поле связанной привычки. Можно заполнить только одно из двух полей.
-bonus
-bonus_habit
-
-В связанные привычки могут попадать только привычки с признаком приятной привычки.
-bonus_habit
-is_bonus_habit
-
-У приятной привычки не может быть вознаграждения или связанной привычки.
-bonus_habit
-bonus_habit
-bonus
-
-"""
-
 
 class MaxValueIntValidator:
     """
@@ -49,3 +31,42 @@ class MaxValueIntValidator:
         if not result:
             message = f"Значение {self.field} не должно превышать {self.max_value}"
             raise serializers.ValidationError(message)
+
+
+def is_bonus_valid(is_bonus_habit, bonus_habit, bonus):
+    """
+    Исключить одновременный выбор связанной привычки и указания вознаграждения.
+    В модели не должно быть заполнено одновременно и поле вознаграждения,
+    и поле связанной привычки. Можно заполнить только одно из двух полей.
+    bonus
+    bonus_habit
+
+    В связанные привычки могут попадать только привычки с признаком приятной привычки.
+    bonus_habit
+    is_bonus_habit
+
+    У приятной привычки не может быть вознаграждения или связанной привычки.
+    is_bonus_habit
+    bonus_habit
+    bonus
+
+    """
+
+    if is_bonus_habit:
+        if bonus_habit or bonus:
+            message = f"У приятной привычки не может быть вознаграждения или связанной привычки."
+            raise serializers.ValidationError(message)
+    else:
+        if bonus and bonus_habit:
+            message = f"Можно заполнить только одно из двух полей (вознаграждение либо связанную привычку)."
+            raise serializers.ValidationError(message)
+
+        if bonus_habit and not bonus_habit.is_bonus_habit:
+            message = f"В связанные привычки могут попадать только привычки с признаком приятной привычки."
+            raise serializers.ValidationError(message)
+
+        if not bonus and not bonus_habit:
+            message = f"Необходимо заполнить одно из полей: вознаграждение, или связанную привычку, или признак приятной привычки."
+            raise serializers.ValidationError(message)
+
+    # print("bonus is valid")
